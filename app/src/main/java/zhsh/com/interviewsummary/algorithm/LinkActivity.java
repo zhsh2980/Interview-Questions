0a6762f1305d8cc796c247521341cb4b3133330d
史强 <shiqiang@julong.cc>
2018-10-11 17:07:30 +0800

    1.上传单链表环的校测代码逻辑,相交点及环长等

--- app/src/main/java/zhsh/com/interviewsummary/algorithm/LinkActivity.java
+++ app/src/main/java/zhsh/com/interviewsummary/algorithm/LinkActivity.java
@@ -14,6 +14,7 @@ import zhsh.com.interviewsummary.data.Node;
 public class LinkActivity extends BaseActivity {
 
     private Button bt_link_reversal;
+    private Button bt_has_ring;
 
     @Override
     protected int setContentViewId() {
@@ -24,6 +25,7 @@ public class LinkActivity extends BaseActivity {
     protected void initView() {
 
         bt_link_reversal = findViewById(R.id.bt_link_reversal);
+        bt_has_ring = findViewById(R.id.bt_has_ring);
 
     }
 
@@ -35,8 +37,104 @@ public class LinkActivity extends BaseActivity {
     @Override
     protected void initListener() {
         bt_link_reversal.setOnClickListener((v) -> startReversal());
+        bt_has_ring.setOnClickListener((v)-> whetherRing());
 
 
+    }
+
+    /**
+     * 判断是否有环:使用步长法判断: 思路,从起点开始分别以2x,1x速度出发两个指针,当遇到null停止,相遇点为null时说明没有环
+     * 如果相遇点不为null,说明有环,注意有两个地方:1.首次相遇点速度为1的指针是进入环的第一圈,切记切记
+     * 证明一下呗:  设 圆环总长为: R , 指针分别设置为 1, 2吧,当1指针首次进入圆环交点时,2指针在圆环位置为 x ,则 2->1 步长为 (R - x)
+     * 此时开始走i步以后相遇则: (R - x) + i = 2i; (原来相距位置加上1走的距离等于 2倍的1走距离)
+     * 则 i = R - x ; (X <= R) 的,所以 i < R (肯定在第一圈相遇)
+     *
+     * 由上可得:
+     * 在第一圈相遇的时候1走的距离为: 设 起始点至环入口位置为 L ,则 1路程为(L + i)
+     * 2走的距离为 (L + nR + i) ,n为圈数
+     * 则: (L + nR + i) = 2(L + i); (切记:n >= 1  -> 2比1先进圈)(2速度为1的2倍,1的总路程 * 2)
+     * 即: L = nR - i = (n - 1)R + (R - i) ; (n >= 1) 我们不管(n - 1)R,只是简单的绕圈数
+     * 所以 L = R - i ; 而首次进入圈相遇的位置为 i ,整个圈长为 R ,
+     * 所以当首次相遇的时候,重新以1为步长,一个以相交点为起始,一个以首节点开始,首次相遇点即为环的起始点;
+     *
+     */
+    private void whetherRing() {
+
+        Node head = new Node(0);
+        Node head1 = new Node(1);
+        Node head2 = new Node(2);
+        Node head3 = new Node(3);
+        Node head4 = new Node(4);
+        Node head5 = new Node(5);
+        Node head6 = new Node(6);
+        Node head7 = new Node(7);
+        Node head8 = new Node(8);
+
+        head.setNext(head1);
+        head1.setNext(head2);
+        head2.setNext(head3);
+        head3.setNext(head4);
+        head4.setNext(head5);
+        head5.setNext(head6);
+        head6.setNext(head7);
+        head7.setNext(head8);
+        head8.setNext(head4);
+
+        //获取步长法在环中的相交点
+        Node node = hasRind(head);
+
+        //查找相交点
+        if (node != null){
+            System.out.println("当前第一次的相交点位: " + node.getData());
+            Node cur1 = head.getNext();
+            Node cur2 = node.getNext();
+
+            while (cur1 != cur2){
+                cur1 = cur1.getNext();
+                cur2 = cur2.getNext();
+            }
+            System.out.println("当前环的起点为:" + cur1.getData());
+
+            int sum = 1 ;
+            Node next = cur1.getNext();
+            while (next != cur1){
+                sum ++ ;
+                next = next.getNext();
+            }
+            System.out.println("环长为:" + sum);
+        }else {
+            System.out.println("此链表没有环");
+        }
+    }
+
+    /**
+     * 判断是否有环
+     * @param head
+     * @return
+     */
+    private Node hasRind(Node head) {
+        //如果链表为null,或者只有一个元素则返回head
+        if (head == null || head.getNext() == null || head.getNext().getNext() == null){
+            return null;
+        }
+
+        //设置首次起点为 1 , 2
+        Node cur1 = head.getNext() ;
+        Node cur2 = head.getNext().getNext() ;
+        while (cur2 != null && cur1 != null){
+            if (cur1 == cur2){ //如果相等就证明是相交点
+                return cur1;
+            }else{  //否则增加1步及2步
+                cur1 = cur1.getNext();
+                if (cur2.getNext() != null){ //切记,2->next 一定要判空
+                    cur2 = cur2.getNext().getNext();
+                }else{
+                    return null;
+                }
+            }
+        }
+
+        return null ;
     }
 
     /**
